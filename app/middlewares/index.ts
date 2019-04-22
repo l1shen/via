569265ce54429c7724874ed70fd3cb8ca9fb2715
WittBulter <nanazuimeng123@gmail.com>
2019-04-22 19:33:30 +0800

    feat: add global middlewares

--- app/middlewares/index.ts
+++ app/middlewares/index.ts
@@ -1,3 +1,2 @@
 export * from './is.allow.content.middleware'
 export * from './find.project.middleware'
-export * from './header.middleware'
--- configs/routing.middlewares.ts
+++ configs/routing.middlewares.ts
@@ -1,12 +1,11 @@
 import { KoaMiddlewareInterface, Middleware } from 'routing-controllers'
 
 @Middleware({ type: 'before' })
-export class CustomErrorHandler implements KoaMiddlewareInterface {
+export class HeaderMiddleware implements KoaMiddlewareInterface {
   async use(context: any, next: (err?: any) => any): Promise<any> {
     context.set('Access-Control-Allow-Methods', 'GET,HEAD,PUT,POST,DELETE,PATCH')
-    context.set('Access-Control-Allow-Origin', context.request.header.origin)
+    context.set('Access-Control-Allow-Origin', context.request.header.origin || context.request.origin)
     context.set('Access-Control-Allow-Headers', ['content-type'])
-    context.set('Access-Control-Max-Age', '20')
     context.set('Access-Control-Allow-Credentials', 'true')
     return next()
   }
