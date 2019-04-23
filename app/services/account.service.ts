ce8453b58fa5b6a2e7d73b904cc25bb0df2a5890
sujiaheng <648952316@qq.com>
2019-04-23 11:38:22 +0800

    refactor(account.service): remove bcrypt

--- app/services/account.service.ts
+++ app/services/account.service.ts
@@ -1,27 +1,29 @@
 import { getMongoRepository, MongoRepository } from 'typeorm'
 import { Service } from 'typedi'
-import { compare, genSalt, hash } from 'bcrypt'
+import { createHash } from 'crypto'
 import { User } from 'entities'
 
 
 @Service()
 export class AccountService {
   repository: MongoRepository<User>
-  
+
   constructor() {
     this.repository = getMongoRepository(User)
   }
-  
+
   async signIn(username: string, password: string): Promise<User | null> {
     const user = await this.repository.findOne({ username })
     if (!user) return null
-    const isSignIn = await compare(password, user.password)
+    const sha256 = createHash('sha256')
+    sha256.update(password)
+    const isSignIn = sha256.digest('hex') === user.password
     return isSignIn ? user : null
   }
 
   async signUp(username: string, password: string): Promise<User> {
-    const salt = await genSalt(10)
-    const hashedPassword = await hash(password, salt)
-    return await this.repository.save({ username, password: hashedPassword })
+    const sha256 = createHash('sha256')
+    const hashedPassword = sha256.update(password)
+    return await this.repository.save({ username, password: hashedPassword.digest('hex') })
   }
 }
